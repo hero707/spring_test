1200483df5a2eafb3e235a1df144f34f64ad4151
hero707 <hero707@gmail.com>
2020-10-10 21:36:01 +0900

    first commit

---
+++ src/main/java/hello/yuko/YukoApplication.java
@@ -0,0 +1,13 @@
+package hello.yuko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YukoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YukoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/yuko/YukoApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.yuko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YukoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
